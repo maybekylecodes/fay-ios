4b46d4e5da5ea867839d54fb231bfb55fa1f6800
Kyle Jennings <Kyle22@gmail.com>
2024-07-12 17:26:48 -0700

    initial commit

---
+++ Fay/FayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayApp.swift
+//  Fay
+//
+//  Created by Kyle Jennings on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
